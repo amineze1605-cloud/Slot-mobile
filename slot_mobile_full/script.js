3ad27cce776a2404153c9290ff94ed2afb991c2e
amineze1605-cloud <amineze1605@gmail.com>
2025-12-11 01:03:03 +0100

    Update script.js

--- slot_mobile_full/script.js
+++ slot_mobile_full/script.js
@@ -199,32 +199,30 @@ async function initPixi() {
 
   try {
     const baseTexture = await loadSpritesheet();
+
+    // 3 colonnes x 4 lignes => 12 symboles
+    const COLS_SHEET = 3;
+    const ROWS_SHEET = 4;
+
     const fullW = baseTexture.width;
     const fullH = baseTexture.height;
 
-    // 3 colonnes x 4 lignes => 12 symboles
-const COLS_SHEET = 3;
-const ROWS_SHEET = 4;
+    const frameW = fullW / COLS_SHEET;
+    const frameH = fullH / ROWS_SHEET;
 
-const fullW = baseTexture.width;
-const fullH = baseTexture.height;
-
-const frameW = fullW / COLS_SHEET;
-const frameH = fullH / ROWS_SHEET;
-
-symbolTextures = [];
-for (let r = 0; r < ROWS_SHEET; r++) {
-  for (let c = 0; c < COLS_SHEET; c++) {
-    const rect = new PIXI.Rectangle(
-      c * frameW,
-      r * frameH,
-      frameW,
-      frameH
-    );
-    const tex = new PIXI.Texture(baseTexture, rect);
-    symbolTextures.push(tex);
-  }
-}
+    symbolTextures = [];
+    for (let r = 0; r < ROWS_SHEET; r++) {
+      for (let c = 0; c < COLS_SHEET; c++) {
+        const rect = new PIXI.Rectangle(
+          c * frameW,
+          r * frameH,
+          frameW,
+          frameH
+        );
+        const tex = new PIXI.Texture(baseTexture, rect);
+        symbolTextures.push(tex);
+      }
+    }
 
     if (!symbolTextures.length) {
       showMessage("Erreur JS : spritesheet vide");
